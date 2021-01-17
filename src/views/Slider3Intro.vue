08070e6ce0bab8ba3868846f81e15be09caee7d2
adrienbannwarth <adrien.bann@gmail.com>
2021-01-17 20:56:05 +0100

    inte sldie intro

--- src/views/Slider3Intro.vue
+++ src/views/Slider3Intro.vue
@@ -4,15 +4,23 @@
     class="u-flex justify-content-center align-items-center"
   >
     <div class="col-8">
-      <h2 class="h2">NOTRE IMPACT<br> SUR LE TEXTILE</h2>
-      <p>
-        Entre 10 000 et 20 000 tonnes de produits textiles sont détruits en
-        France soit l’équivalent du poids d’un ou deux Tour Eiffel
-      </p>
-      <p>
-        1,2 milliards de tonnes de gaz à effet de serre par an à cause de la
-        production et le transport des textiles
-      </p>
+      <h2 class="h2 u-yellow u-center u-yellow">TEXTILE</h2>
+      <p>Notre impact sur l'environnement à travers le textile ...</p>
+      <div class="u-flex justify-content-between align-items-center">
+        <img style="width: 20%" :src="'./img/eiffel.png'" alt="" />
+        <p style="width: 70%">
+          Entre <span class="u-yellow text--xl">10 000</span> et
+          <span class="u-yellow text--xl">20 000</span> tonnes de produits textiles sont
+          détruits en France soit l’équivalent du poids d’un ou deux Tour Eiffe
+        </p>
+      </div>
+      <div class="u-flex justify-content-between align-items-center">
+        <p style="width: 70%">
+          <span class="u-yellow text--xl">1,2 </span>milliards de tonnes de gaz à effet
+          de serre par an à cause de la production et le transport des textiles
+        </p>
+        <img style="width: 20%" :src="'./img/usine.png'" alt="" />
+      </div>
     </div>
   </div>
 </template>
