2575f9d322c6be61e94145776a281c166ca8ead7
adrienbannwarth <adrien.bann@gmail.com>
2021-01-22 09:31:14 +0100

    add anim data SlideIntro

--- src/views/Slider2Intro.vue
+++ src/views/Slider2Intro.vue
@@ -4,25 +4,29 @@
     class="u-flex justify-content-center align-items-center slider-2"
   >
     <div class="col-8">
-      <h2 class="h2 u-red u-center">
-        ALIMENTATION
-      </h2>
-      <p class="u-center">Notre impact sur l'environnement à travers l'alimentation</p>
+      <h2 class="h2 u-red u-center">ALIMENTATION</h2>
+      <p class="u-center">
+        Notre impact sur l'environnement à travers l'alimentation
+      </p>
       <div class="u-flex justify-content-between align-items-center">
         <img style="width: 20%" :src="'./img/earth-red.png'" alt="" />
         <p style="width: 70%">
-          <span class="u-red text--xl">1/3</span> des aliments produits sur la planète
-          sont jetés sans être consommés.
+          <span class="u-red text--xl">1/3</span> des aliments produits sur la
+          planète sont jetés sans être consommés.
         </p>
       </div>
       <br />
       <div class="u-flex justify-content-between align-items-center">
         <p style="width: 70%">
-          <span class="u-red text--xl"> 75%</span> des terres agricoles du globe sont
-          dédiés à la nourriture du bétail..
+          <span id="counter" class="u-red text--xl"> 75%</span> des terres agricoles du globe
+          sont dédiés à la nourriture du bétail..
         </p>
         <img style="width: 20%" :src="'./img/steak.png'" alt="" />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+
+</script>
